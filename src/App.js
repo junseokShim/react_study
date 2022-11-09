b5cc832c6aa2419ddb990ad7398f251a8bddfdfa
junseokShim <shimjunseok@simjunseog-ui-MacBookPro-2.local>
2022-11-09 20:46:56 +0900

    modified contents : component date : 11.09

--- src/App.js
+++ src/App.js
@@ -7,12 +7,29 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props){
+    // componet init (component 동작시 최초로 실행됨)
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'world wide web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
+        {id:2, title:'CSS', desc:'CSS is for design ...'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive ...'}
+      ]
+    }
+  }
+
   render(){
     return (
       <div className = "App">
-        <Subject title="WEB" sub="world wide web!!"></Subject>
+        <Subject 
+        title={this.state.subject.title} // 상위 state값을 하위 props의 값으로 사용
+        sub={this.state.subject.sub}>
+        </Subject>
+        
         <Subject title="React" sub="For UI!!"></Subject>
-        <Toc></Toc>
+        <Toc data={this.state.contents}></Toc>
         <Contents title="HTML" desc="HTML is HyperText Markup Language."></Contents>
       </div>
     );
