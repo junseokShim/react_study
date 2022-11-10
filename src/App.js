9eb89a42ba016595646fdb735924495b993b8986
junseokShim <shimjunseok@simjunseog-ui-MacBookPro-2.local>
2022-11-10 22:16:49 +0900

    modified content : component event date : 11.10

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ class App extends Component {
     // componet init (component 동작시 최초로 실행됨)
     super(props);
     this.state = {
+      mode:'read',
+      seleceted_content_id:2,
       subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:"Welcome", desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is HyperText ...'},
         {id:2, title:'CSS', desc:'CSS is for design ...'},
@@ -19,20 +22,56 @@ class App extends Component {
       ]
     }
   }
-
-  render(){
+  render() {
+    var _title, _desc = null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }
+    else if(this.state.mode==='read'){
+      var i = 0;
+      while(i<this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id===this.state.seleceted_content_id){
+          _title=data.title;
+          _desc=data.desc;
+          break;
+        }
+        i=i+1;
+      }
+    }
     return (
       <div className = "App">
         <Subject 
         title={this.state.subject.title} // 상위 state값을 하위 props의 값으로 사용
-        sub={this.state.subject.sub}>
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+          if(this.state.mode==='welcome'){
+            this.setState({
+              mode:'read'
+            })
+          }
+          else if(this.state.mode==='read'){
+            this.setState({
+              mode:'welcome'
+            })
+          }
+        }.bind(this)}
+        >
         </Subject>
         
         <Subject title="React" sub="For UI!!"></Subject>
-        <Toc data={this.state.contents}></Toc>
-        <Contents title="HTML" desc="HTML is HyperText Markup Language."></Contents>
+        <Toc onChangePage={function(id){
+          this.setState({
+            mode:"read",
+            seleceted_content_id:Number(id)
+          });
+        }.bind(this)}
+        data={this.state.contents}></Toc>
+        <Contents title={_title} desc={_desc}></Contents>
       </div>
     );
   }
 }
+
 export default App;
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,8 +4,13 @@ class Subject extends Component {
     render() { // class 안에서는 function을 생략해도 됨
       return (
         <header>
-          <h1>{this.props.title}</h1>
-          {this.props.sub}
+          <h1>
+            <a href='/' onClick={function(e){
+              e.preventDefault();
+              this.props.onChangePage();
+            }.bind(this)}>{this.props.title}
+            </a>
+          </h1>
         </header>
       );
     }
